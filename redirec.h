7a3bc61f2dbd0ae4446b31be8251c2d6f6227f0b
Winston Peng <panda501wp@gmail.com>
2019-12-19 11:16:41 -0500

    This is a terrible idea

--- redirec.h
+++
@@ -1,4 +0,0 @@
-int my_open(char *file, int flag);
-int my_new_file_open(char *filename, int flag, int permissions);
-int my_write(int file, int *buffer, int size);
-int should_redirect(char *term_in);
